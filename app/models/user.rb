665e299dfe86217f902f4d6522613c6a1782aee2
francois guerin <francois.theo.guerin@outlook.com>
2017-04-24 17:23:37 -0300

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
+  has_many hangouts, dependent: :destroy
+  has_many confirmations, dependent: :destroy
+
+  # after_create :send_welcome_email
+  # before_save :set_avatar_placeholder
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  # mount_uploader :photo, PhotoUploader
+
+# private
+
+#   def set_avatar_placeholder
+#     self.avatar_url = "avatar-placeholder.png" unless self.avatar_url.present?
+#   end
+
+#   def send_welcome_email
+#     UserMailer.welcome(self).deliver_now
+#   end
+
 end
