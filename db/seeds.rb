d606f3fcf2cc98acd64005115fd9d7a773cc03ca
christophe vallerin <christophe.vallerin@hotmail.com>
2017-04-25 16:27:11 -0300

    work on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+a = User.new( first_name: "Abilio",
+  last_name: "Diniz",
+  facebook_picture_url: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAALuAAAAJDllYmNhNDQwLTUxMWQtNGM5MS05MTVmLTg5NTg5NmM0MjNkMg.jpg",
+  email: "ad@gmail.com",
+  encrypted_password: "$2a$11$b1UALeSIShywjDrI2ortU.Nn7sFrTVrciNLsn7JEfthMv9fX5nsWy"
+)
+a.save
