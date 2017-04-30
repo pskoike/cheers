2027ab5cd74479c40e8795e824af7d508a16e973
Pedro Koike <ps.koike@gmail.com>
2017-04-30 00:31:55 -0300

    api explorer section done

--- app/services/places_api.rb
+++ app/services/places_api.rb
@@ -1,49 +1,35 @@
 class PlacesApi
-  CATEGORIES = {
-    food: "4d4b7105d754a06374d81259",
-    bar: "4bf58dd8d48988d116941735",
-    brewery: "50327c8591d4c4b30a586d5d",
-    lounge: "4bf58dd8d48988d121941735",
-    nightclub: "4bf58dd8d48988d11f941735",
-  }
+
   def initialize(lat, lng, hangout)
     @lat = lat
     @lng = lng
     @hangout = hangout
-    @categories = CATEGORIES
   end
 
   def fetch_places
-    # category = @categories[@hangout.category.to_sym]
-    # p category
-    # url = "https://api.foursquare.com/v2/venues/search?oauth_token=TO4OQUXI30PHNRCY1LBG2AG2FVESICUW4OE1RZ4S350DPRO5&v=20170401&ll=#{@lat},#{@lng}&intent=checkin&radius=2000&categoryId=#{category}"
-    url = "https://api.foursquare.com/v2/venues/search?v=20170401&ll=#{@lat},#{@lng}&radius=2000&query=#{@hangout.category}&oauth_token=TO4OQUXI30PHNRCY1LBG2AG2FVESICUW4OE1RZ4S350DPRO5"
-
     # URl to explore by Section
-    # url = "https://api.foursquare.com/v2/venues/explore?ll=-23.5593651,-46.6770567&radius=2000&section=food&client_id=NHMKJWILK1SJM00EQ1WNDI5EQS3TYNVUOQFBUARSGDTIVJK1&client_secret=JFZQACYBUYN4TDKJUWB5X45RVV3PRLLBXCPBEEUVZ2MJKLET&v=20170101"
-
-
-    # "https://api.foursquare.com/v2/venues/search?v=20161016&near=Sao%20Paulo%2C%20SP&categoryId=#{category}&client_id={ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}"
+    url = "https://api.foursquare.com/v2/venues/explore?ll=#{@lat},#{@lng}&radius=2000&section=drinks&client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&v=20170101"
     url.gsub!('"')
     venues_from_category = RestClient.get url
     # list of venues from each category
     venues = JSON.parse(venues_from_category)
-    venues["response"]["venues"].map do | v |
-       v["id"]
+    # venues["response"]["venues"].map do | v |
+    venues["response"]["groups"][0]['items'].map do | v |
+       v['venue']["id"]
     end
   end
 
   def find_places(ids)
-    ids.map do |id|
+    results = ids.map do |id|
       p = Place.find_by(fsq_id: id)
       if !p
-        url ="https://api.foursquare.com/v2/venues/#{id}?v=20170401&oauth_token=TO4OQUXI30PHNRCY1LBG2AG2FVESICUW4OE1RZ4S350DPRO5"
+        # url ="https://api.foursquare.com/v2/venues/#{id}?v=20170401&oauth_token=TO4OQUXI30PHNRCY1LBG2AG2FVESICUW4OE1RZ4S350DPRO5"
+        url ="https://api.foursquare.com/v2/venues/#{id}?v=20170401&&client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}"
         url.gsub!('"')
         response_venue = RestClient.get url
         response_venue = JSON.parse(response_venue)
         venue_hash = response_venue['response']['venue']
-        # if @categories.has_value?(venue_hash['categories'][0]['id'])
-          venue = Place.create(
+        venue = Place.create(
           name: venue_hash['name'],
           address: "#{venue_hash['location']['address']}, #{venue_hash['location']['city']}, #{venue_hash['location']['state']} ",
           longitude:venue_hash['location']['lat'],
@@ -54,14 +40,12 @@ class PlacesApi
           fsq_url: venue_hash['canonicalUrl'],
           fsq_cat_id: venue_hash['categories'][0]['id'],
           # photo_url: venue_details['response']['venue']['photos']['groups'][0]['items'][0]['prefix'] + "200x200" + venue_details['response']['venue']['photos']['groups'][0]['items'][0]['suffix'] ,
-          )
-
-        # end
+        )
       else
         p
       end
-
     end
-
+    results.sort! {|x,y| y.rating <=> x.rating }
+    results.take(15).sample(5) #Take the first 15 and sample.
   end
 end
