0c8069924d5efbc0d59966e6341d20bcf566fc79
francois guerin <francois.theo.guerin@outlook.com>
2017-04-30 16:49:57 -0300

    maps ok let do sthg to save more data and not do calculation each time

--- app/controllers/hangouts_controller.rb
+++ app/controllers/hangouts_controller.rb
@@ -140,30 +140,33 @@ private
     if @hangout.status == "confirmations_on_going"
       @render = 'confirmationfollowup'
 
+      #Building array of markets with leaving lat/lng of the confirmations
       @confirmations = Confirmation.all.where('hangout_id = ?',@hangout.id)
       @confirmation_markers = []
       @confirmations.each do |confirmation|
-        @confirmation_markers << {lat: confirmation.latitude, lng: confirmation.longitude}
+        @confirmation_markers << {lat: confirmation.latitude, lng: confirmation.longitude, mode: confirmation.transportation}
       end
 
-      nb = @confirmations.count
-      avg_lat = @confirmations.reduce(0){ |sum, el| sum + el.latitude }.to_f / nb
-      avg_ln = @confirmations.reduce(0){ |sum, el| sum + el.longitude }.to_f / nb
-      @center = {lat: avg_lat, lng: avg_ln}
-
-      delta_lat = (@confirmations.max_by {|x| x.latitude}).latitude - (@confirmations.min_by {|x| x.latitude}).latitude
-      delta_lng = (@confirmations.max_by {|x| x.longitude}).longitude - (@confirmations.min_by {|x| x.longitude}).longitude
-
-      raw_radius = (delta_lat + delta_lng) / 4
-
-      magic_factor = 20000
-
-      @radius = raw_radius * magic_factor
-
-      @hangout.latitude = @center[:lat]
-      @hangout.longitude = @center[:lng]
-      @hangout.radius = @radius
+      zone = fetch_zone(@confirmation_markers)
+      @center = zone[:center]
+      @radius= zone[:radius]
+      @hangout.latitude = zone[:center][:lat]
+      @hangout.longitude = zone[:center][:lng]
+      @hangout.radius = zone[:radius]
       @hangout.save
+
+      #Geting distance, duration to the center for each marker
+      @confirmation_markers.each do |confirmation_marker|
+        confirmation_marker = get_direction(confirmation_marker, @center, @hangout.date)
+      end
+      #recalcute center adjusting lat, lng with duration
+      @adj_center = fetch_adjusted_zone(@confirmation_markers, @center)
+
+      #one more iteration for accuracy
+      @confirmation_markers.each do |confirmation_marker|
+        confirmation_marker = get_direction(confirmation_marker, @adj_center,@hangout.date)
+      end
+      @adj_center2 = fetch_adjusted_zone(@confirmation_markers, @adj_center)
 
     elsif @hangout.status == "vote_on_going"
       @render = 'vote_option'
@@ -186,4 +189,43 @@ private
     venues = fetch.fetch_places
     fetch.find_places(venues)
   end
+
+  def fetch_zone(confirmation_markers)
+    nb = confirmation_markers.count
+    avg_lat = confirmation_markers.reduce(0){ |sum, el| sum + el[:lat]}.to_f / nb
+    avg_ln = confirmation_markers.reduce(0){ |sum, el| sum + el[:lng]}.to_f / nb
+    center = {lat: avg_lat, lng: avg_ln}
+
+    delta_lat = (confirmation_markers.max_by {|x| x[:lat]})[:lat] - (confirmation_markers.min_by {|x| x[:lat]})[:lat]
+    delta_lng = (confirmation_markers.max_by {|x| x[:lng]})[:lng] - (confirmation_markers.min_by {|x| x[:lng]})[:lng]
+    raw_radius = (delta_lat + delta_lng) / 4
+    magic_factor = 20000 #factor to size sensibility of the radius vs. distance between participants
+    min_radius = 600
+    radius = [raw_radius * magic_factor, min_radius].max
+   return {center: center, radius: radius}
+  end
+
+  def fetch_adjusted_zone(confirmation_markers, center)
+    div = confirmation_markers.reduce(0){ |sum, el| sum + el[:duration]}
+    avg_lat = confirmation_markers.reduce(0){ |sum, el| sum + el[:lat]*el[:duration]}.to_f / div
+    avg_ln = confirmation_markers.reduce(0){ |sum, el| sum + el[:lng]*el[:duration]}.to_f / div
+    weighted_center = {lat: avg_lat, lng: avg_ln}
+    adj_center = {lat: (weighted_center[:lat] + center[:lat]) / 2 , lng: (weighted_center[:lng] + center[:lng]) / 2 }
+    return adj_center
+  end
+
+  def get_direction(departure, destination, departure_time)
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=#{departure[:lat]},#{departure[:lng]}&destinations=#{destination[:lat]},#{destination[:lng]}&departure_time=#{departure_time.to_i}&mode=#{departure[:mode].downcase}&key=#{ENV['GOOGLE_API_SERVER_KEY']}"
+    url.gsub!('"')
+    direction = RestClient.get url
+    direction_info = JSON.parse(direction)
+    departure[:distance] = direction_info["rows"][0]["elements"][0]["distance"]["value"]
+    if departure[:mode] == 'DRIVING'
+      departure[:duration] = direction_info["rows"][0]["elements"][0]["duration_in_traffic"]["value"]
+    else
+      departure[:duration] = direction_info["rows"][0]["elements"][0]["duration"]["value"]
+    end
+    return departure
+  end
+
 end
