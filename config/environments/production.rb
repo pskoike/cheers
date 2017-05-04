e61f34641b6cddf86db8251ab070581945d5f235
francois guerin <francois.theo.guerin@outlook.com>
2017-05-04 18:51:59 -0300

    new hg new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://cheers64.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://www.kippis.website" }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
